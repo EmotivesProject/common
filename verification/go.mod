33f5f8dcb7fa8040f1baed77edd50effeaf9dfc7
Tom Bowyer <tom4310@gmail.com>
2021-04-17 10:31:26 +1000

    Finished off verification

--- verification/go.mod
+++ verification/go.mod
@@ -2,4 +2,7 @@ module github.com/TomBowyerResearchProject/common/verification
 
 go 1.13
 
-require github.com/TomBowyerResearchProject/common/logger v0.0.0-20210415091410-088ae6038e62 // indirect
+require (
+	github.com/TomBowyerResearchProject/common/logger v0.0.0-20210415091410-088ae6038e62 // indirect
+	github.com/TomBowyerResearchProject/common/response v0.0.0-20210415091410-088ae6038e62
+)
---
+++ verification/verification.go
@@ -0,0 +1,104 @@
+package verification
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/TomBowyerResearchProject/common/response"
+)
+
+type key string
+
+const (
+	userID key = "username"
+)
+
+var (
+	errUnauthorised    = errors.New("Not authorised")
+	verificationConfig VerificationConfig
+)
+
+type VerificationUser struct {
+	Username string `json:"username"`
+}
+
+type VerificationConfig struct {
+	VerificationURL     string
+	AuthorizationSecret string
+}
+
+func Init(cfg VerificationConfig) {
+	verificationConfig = cfg
+}
+
+func VerifyJTW() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			client := &http.Client{}
+
+			header := r.Header.Get("Authorization")
+
+			req, err := http.NewRequest("GET", verificationConfig.VerificationURL, nil)
+			if err != nil {
+				response.MessageResponseJSON(w, http.StatusBadRequest, response.Message{
+					Message: err.Error(),
+				})
+				return
+			}
+
+			req.Header.Add("Authorization", header)
+			resp, err := client.Do(req)
+			if err != nil {
+				response.MessageResponseJSON(w, http.StatusBadRequest, response.Message{
+					Message: err.Error(),
+				})
+				return
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode == http.StatusOK {
+				bodyBytes, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					response.MessageResponseJSON(w, http.StatusBadRequest, response.Message{
+						Message: err.Error(),
+					})
+					return
+				}
+
+				var dat response.Response
+				var user VerificationUser
+				_ = json.Unmarshal(bodyBytes, &dat)
+				body, _ := json.Marshal(dat.Result)
+
+				_ = json.Unmarshal(body, &user)
+				ctx := context.WithValue(r.Context(), userID, user.Username)
+				next.ServeHTTP(w, r.WithContext(ctx))
+				return
+			}
+
+			response.MessageResponseJSON(w, http.StatusBadRequest, response.Message{
+				Message: errUnauthorised.Error(),
+			})
+		})
+	}
+}
+
+func VerifyToken() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			header := r.Header.Get("Authorization")
+
+			if header == verificationConfig.AuthorizationSecret {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			response.MessageResponseJSON(w, http.StatusBadRequest, response.Message{
+				Message: errUnauthorised.Error(),
+			})
+		})
+	}
+}
