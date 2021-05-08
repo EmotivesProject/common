0ee3c4b91bb23830d917070b1751409fd4f22141
Tom Bowyer <tom4310@gmail.com>
2021-05-08 10:04:37 +1000

    Add postid

--- notification/main.go
+++ notification/main.go
@@ -11,6 +11,7 @@ type Notification struct {
 	Title    string `json:"title"`
 	Message  string `json:"message"`
 	Link     string `json:"link"`
+	PostID   *int   `json:"post_id,omitempty"`
 }
 
 func SendEvent(url, authSecret string, notif Notification) error {
