ffb11a446ac7f7329e5c7c9323fa401c5ae5c3bb
Tom Bowyer <tom4310@gmail.com>
2021-09-19 08:34:04 +1000

    Make changes to allowed origins

--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -1,26 +1,40 @@
 package middlewares
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+
+	"github.com/TomBowyerResearchProject/common/response"
+)
 
 type Config struct {
-	AllowedOrigin  string
+	AllowedOrigins string
 	AllowedHeaders string
 	AllowedMethods string
 }
 
 var (
-	config Config
+	config              Config
+	splitAllowedOrigins []string
 )
 
 func Init(middlewareConfig Config) {
 	config = middlewareConfig
+	splitAllowedOrigins = strings.Split(config.AllowedOrigins, ",")
 }
 
 func SimpleMiddleware() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			origin := r.Header.Get("Origin")
+			if in := stringInSlice(origin, splitAllowedOrigins); !in {
+				response.MessageResponseJSON(w, false, http.StatusMethodNotAllowed, response.Message{Message: "no allowed"})
+
+				return
+			}
+
 			w.Header().Add("Content-Type", "application/json")
-			w.Header().Set("Access-Control-Allow-Origin", config.AllowedOrigin)
+			w.Header().Set("Access-Control-Allow-Origin", origin)
 			w.Header().Set("Access-Control-Allow-Methods", config.AllowedMethods)
 			w.Header().Set("Access-Control-Allow-Headers", config.AllowedHeaders)
 
@@ -31,4 +45,14 @@ func SimpleMiddleware() func(http.Handler) http.Handler {
 			next.ServeHTTP(w, r)
 		})
 	}
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+
+	return false
 }
