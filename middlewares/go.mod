655b37d3d519ffa6e4f54eeebb9a6c6b43b4692f
Tom Bowyer <tom4310@gmail.com>
2021-09-19 10:54:47 +1000

    Attempt to re add cors

--- middlewares/go.mod
+++ middlewares/go.mod
@@ -1,3 +1,5 @@
 module github.com/TomBowyerResearchProject/common/middlewares
 
 go 1.13
+
+require github.com/TomBowyerResearchProject/common/response v0.0.0-20210918225212-3eb04f0c81a1
--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -1,26 +1,38 @@
 package middlewares
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 type Config struct {
-	AllowedOrigin  string
+	AllowedOrigins string
 	AllowedHeaders string
 	AllowedMethods string
 }
 
 var (
-	config Config
+	config              Config
+	splitAllowedOrigins []string
 )
 
 func Init(middlewareConfig Config) {
 	config = middlewareConfig
+	splitAllowedOrigins = strings.Split(config.AllowedOrigins, ",")
 }
 
 func SimpleMiddleware() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// Possibly set Access-Control-Allow-Origin to something other than the first
+			origin := r.Header.Get("Origin")
+			if index := stringInSlice(origin, splitAllowedOrigins); index != -1 {
+				w.Header().Set("Access-Control-Allow-Origin", splitAllowedOrigins[index])
+			} else {
+				w.Header().Set("Access-Control-Allow-Origin", splitAllowedOrigins[0])
+			}
+
 			w.Header().Add("Content-Type", "application/json")
-			w.Header().Set("Access-Control-Allow-Origin", config.AllowedOrigin)
 			w.Header().Set("Access-Control-Allow-Methods", config.AllowedMethods)
 			w.Header().Set("Access-Control-Allow-Headers", config.AllowedHeaders)
 
@@ -31,4 +43,14 @@ func SimpleMiddleware() func(http.Handler) http.Handler {
 			next.ServeHTTP(w, r)
 		})
 	}
+}
+
+func stringInSlice(a string, list []string) int {
+	for idx, b := range list {
+		if b == a {
+			return idx
+		}
+	}
+
+	return -1
 }
